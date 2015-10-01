76c8a79116e5ed7687b9b3748db38769dab0e599
Mischa Radenovic <miroslav.radenovic@gmail.com>
2015-10-01 00:53:44 -0400

    Add content script for "Extra Stop" page
    Add following functionality:
     * An input field with address autocomplete
     * A function to get list of extra stops
    
    The input field helps easier adddress entry for extra stop, and the
    list of extrastops is used for route calculation in "Charges" page.

---
+++ extra_stop.js
@@ -0,0 +1,71 @@
+/**
+ * Initializes extra_stop content scrip
+ *
+ */
+function initExtraStop() {
+  // append address input field
+  $('textarea')
+    .attr('id','textarea')
+    .attr('title','Enclose the address in square brackets, i.e. [NY 11101]')
+    .parent().parent().before($(document.createElement('tr'))
+    .append($(document.createElement('td'))
+      .attr('width','100%')
+      .attr('align','center')
+      .append($(document.createElement('input'))
+        .attr('id','address-input')
+        .attr('type','text')
+        .attr('size','60')
+        .attr('placeholder','Enter an extra stop location and press enter')
+      )
+    )
+  )
+
+  // Suppress submit on enter
+  $('form').on('keyup keypress', function(e) {
+    var code = e.keyCode || e.which;
+    if (code == 13) {
+      e.preventDefault();
+      return false;
+    }
+  });
+
+  // Load extra_stop.js into script tag
+  $('head')
+    .append($('<script>')
+      .load(chrome.extension.getURL('extra_stop_maps.js'), initMapsAPI())
+    )
+}
+
+/**
+ * Initializes Google Maps API
+ *
+ */
+function initMapsAPI() {
+  // append Google Maps API script tag
+  $('head')
+    .append($('<script>')
+      .attr('type','text/javascript')
+      .attr('src','https://maps.googleapis.com/maps/api/js?signed_in=true&libraries=places&callback=initAutocomplete')
+    )
+}
+
+function getExtraStops() {
+  var re = /\[.*\]/g;
+  var extraStops = $('textarea').val().match(re);
+  return extraStops;
+}
+
+function init() {
+  initExtraStop();
+/*
+  // TODO Load script if option si checked
+  chrome.storage.sync.get('mapsAutocomplete', function(items) {
+    console.log('mapsAutocomplete: ',items.mapsAutocomplete)
+    if (items.mapsAutocomplete) {
+      loadScriptFromFile();
+    }
+  });
+*/
+}
+
+init();
---
+++ extra_stop_maps.js
@@ -0,0 +1,16 @@
+/**
+ * Initializes autocomplete object
+ */
+function initAutocomplete() {
+  console.log('initAutocomplete executed!')
+  var addressInput = document.getElementById('address-input');
+  var autocomplete = new google.maps.places.Autocomplete(
+      addressInput,
+      {types: ['address']});
+  autocomplete.addListener('place_changed', function () {
+      var textArea = document.getElementById('textarea');
+      textArea.value += '\n[' + autocomplete.getPlace().formatted_address + ']\n';
+      addressInput.value = '';
+    });
+}
+
