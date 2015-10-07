edda5b91d0182dc26d1209d06d4fd474ba78b535
Mischa Radenovic <miroslav.radenovic@gmail.com>
2015-10-07 17:23:13 -0400

    Rewrite entry_form.js to use jQuery

--- entry_form.js
+++ entry_form.js
@@ -1,42 +1,35 @@
 /**
- * Loads Script from file and calls initialization function.
+ * Initializes entry_form content scrip
  *
  */
-function loadScriptFromFile() {
-  var fileURL = chrome.extension.getURL('entry_form_maps.js');
-  var request = new XMLHttpRequest();
-  request.open('GET', fileURL);
-  request.onloadend = function() {
-    initMapsAPI(request.responseText)
-  }
-  request.send();
-
+function initEntryForm() {
+  // Load entry_form_maps.js into script tag
+  $('head')
+    .append($('<script>')
+      .load(chrome.extension.getURL('entry_form_maps.js'), initMapsAPI())
+    )
 }
 
 /**
- * Initializes Google Maps API by loading scripts into current page.
+ * Initializes Google Maps API
  *
  */
-function initMapsAPI(js) {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.textContent = js;
-  document.body.appendChild(script);
-
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = "https://maps.googleapis.com/maps/api/js?signed_in=true&libraries=places&callback=initAutocomplete";
-  script.async = true;
-  script.defer = true;
-  document.body.appendChild(script);
+function initMapsAPI() {
+  // append Google Maps API script tag
+  $('head')
+    .append($('<script>')
+      .attr('type','text/javascript')
+      .attr('src','https://maps.googleapis.com/maps/api/js?signed_in=true&libraries=places&callback=initAutocomplete')
+    )
 }
+
 
 function init() {
   // Load script if option si checked
   chrome.storage.sync.get('mapsAutocomplete', function(items) {
     console.log('mapsAutocomplete: ',items.mapsAutocomplete)
     if (items.mapsAutocomplete) {
-      loadScriptFromFile();
+      initEntryForm();
     }
   });
 }
