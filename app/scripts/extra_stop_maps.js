c0ffff284cf1b13a9c4ebad828263f2a6dfe925a
Mischa Radenovic <miroslav.radenovic@gmail.com>
2015-12-13 12:41:17 -0500

    Fix jshint warnings and beautify

--- app/scripts/extra_stop_maps.js
+++ app/scripts/extra_stop_maps.js
@@ -1,16 +1,20 @@
+/* jshint camelcase: false */
+/* exported initAutocomplete */
+/* globals google*/
+
 /**
  * Initializes autocomplete object
  */
 function initAutocomplete() {
-  console.log('initAutocomplete executed!')
+  console.log('initAutocomplete executed!');
   var addressInput = document.getElementById('address-input');
   var autocomplete = new google.maps.places.Autocomplete(
-      addressInput,
-      {types: ['address']});
-  autocomplete.addListener('place_changed', function () {
-      var textArea = document.getElementById('textarea');
-      textArea.value += '\n[' + autocomplete.getPlace().formatted_address + ']\n';
-      addressInput.value = '';
+    addressInput, {
+      types: ['address']
     });
+  autocomplete.addListener('place_changed', function () {
+    var textArea = document.getElementById('textarea');
+    textArea.value += '\n[' + autocomplete.getPlace().formatted_address + ']\n';
+    addressInput.value = '';
+  });
 }
-
