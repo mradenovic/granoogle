781e33fdd333117ccb16c45c021e945725139729
Mischa Radenovic <miroslav.radenovic@gmail.com>
2015-12-13 18:33:58 -0500

    Remove 'babel' task from Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,7 +37,8 @@ module.exports = function (grunt) {
       },
       js: {
         files: ['<%= config.srcScript %>/{,*/}*.js'],
-        tasks: ['jshint', 'babel'],
+        // tasks: ['jshint', 'babel'],
+        tasks: ['jshint'],
         options: {
           livereload: '<%= connect.options.livereload %>'
         }
